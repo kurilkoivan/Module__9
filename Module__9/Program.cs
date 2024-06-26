d971a9c729d634f3a005cf721a7bb3c39a3dd423
kurilkoivan <kurilkoivan7@gmail.com>
2024-06-26 18:07:26 +0500

    9.2.2

--- Module__9/Program.cs
+++ Module__9/Program.cs
@@ -1,6 +1,18 @@
 ﻿static void Main(string[] args)
 {
-    Exception exception = new Exception("какая то ошибка");
-    exception.Data.Add("дата создания исключения", DateTime.Now);
-    exception.HelpLink = "www.удачи.kz";
+    try
+    {
+        Console.WriteLine("Процесс начат.");
+        throw new ArgumentOutOfRangeException();
+    }
+
+    catch (ArgumentOutOfRangeException ex)
+    {
+        Console.Write(ex.Message);
+    }
+
+    finally
+    {
+        Console.Read();
+    }
 }
