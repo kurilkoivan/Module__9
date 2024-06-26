29391003b5dfcedf83299cd1c374fb1881e13f98
kurilkoivan <kurilkoivan7@gmail.com>
2024-06-26 18:29:44 +0500

    9.3.12

--- Module__9/Program.cs
+++ Module__9/Program.cs
@@ -2,40 +2,15 @@
 {
     class Program
     {
-            
-            
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            
-
-            Action showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
-
-            Func<int,int,int,int> sumDelegate = Sum;
-            int result = sumDelegate.Invoke(1, 30, 120);
-            Console.WriteLine(result);
-
-            Predicate<string> checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("skill_factory");
-            Console.WriteLine(status);
-
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
             Console.Read();
-        }
-
-        static void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-        static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
-        }
-
-        static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
         }
     }
 }
