eceb1f3aa722cb1b0c544f6459aee327baad4d6f
kurilkoivan <kurilkoivan7@gmail.com>
2024-06-26 18:31:50 +0500

    9.3.15

--- Module__9/Program.cs
+++ Module__9/Program.cs
@@ -2,14 +2,15 @@
 {
     class Program
     {
-        delegate void ShowMessageDelegate(string _message);
+        delegate int RandomNumberDelegate();
         static void Main(string[] args)
         {
-            ShowMessageDelegate showMessageDelegate = (string _message) =>
+            RandomNumberDelegate randomNumberDelegate = () =>
             {
-                Console.WriteLine(_message);
+                return new Random().Next(0, 100);
             };
-            showMessageDelegate.Invoke("Hello World!");
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
             Console.Read();
         }
     }
